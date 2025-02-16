ddeae04d28be2e506ceb1a47ebc0f760f3cd4d37
Terry0532 <t3rrygong@gmail.com>
2025-02-16 15:17:12 -0700

    refactor

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css';
-import Game from './components/game.js'
-
+import Game from './components/game.tsx'
 
 ReactDOM.render(
   <Game />,
---
+++ client/src/socket.js
@@ -0,0 +1,6 @@
+import socketIOClient from 'socket.io-client';
+
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = 'http://127.0.0.1:4444';
+
+export const socket = socketIOClient(URL);
