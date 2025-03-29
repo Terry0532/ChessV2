c2b6682d46f44512da98378099823963679404d3
Terry0532 <t3rrygong@gmail.com>
2025-03-29 17:49:25 -0600

    update knight test

--- client/src/pieces/knight.test.js
+++ client/src/pieces/knight.test.js
@@ -26,7 +26,7 @@ describe('Knight', () => {
     const position = 27;
     board[position] = knight;
     const enemyPositions = [10, 12, 17, 21, 33, 37, 42, 44];
-    enemyPositions.forEach(pos => {
+    enemyPositions.forEach((pos) => {
       board[pos] = new Knight(Player.Black);
     });
     const moves = knight.possibleMoves(position, board);
@@ -39,7 +39,7 @@ describe('Knight', () => {
     const position = 27;
     board[position] = knight;
     const friendlyPositions = [10, 12, 17, 21, 33, 37, 42, 44];
-    friendlyPositions.forEach(pos => {
+    friendlyPositions.forEach((pos) => {
       board[pos] = new Knight(Player.White);
     });
     const moves = knight.possibleMoves(position, board);
@@ -52,19 +52,37 @@ describe('Knight', () => {
       { pos: 0, expected: [10, 17] },
       { pos: 7, expected: [13, 22] },
       { pos: 56, expected: [41, 50] },
-      { pos: 63, expected: [46, 53] }, 
-      { pos: 8, expected: [18, 25, 2] }, 
+      { pos: 63, expected: [46, 53] },
+      { pos: 8, expected: [18, 25, 2] },
       { pos: 16, expected: [1, 10, 26, 33] },
       { pos: 24, expected: [9, 18, 34, 41] },
       { pos: 32, expected: [17, 26, 42, 49] },
       { pos: 40, expected: [25, 34, 50, 57] },
-      { pos: 48, expected: [33, 42, 58] }, 
-      { pos: 55, expected: [38, 45, 61] }, 
-      { pos: 47, expected: [30, 37, 53, 62] }, 
-      { pos: 39, expected: [22, 29, 45, 54] }, 
-      { pos: 31, expected: [14, 21, 37, 46] }, 
-      { pos: 23, expected: [6, 13, 29, 38] }, 
-      { pos: 15, expected: [5, 21, 30] }, 
+      { pos: 48, expected: [33, 42, 58] },
+      { pos: 55, expected: [38, 45, 61] },
+      { pos: 47, expected: [30, 37, 53, 62] },
+      { pos: 39, expected: [22, 29, 45, 54] },
+      { pos: 31, expected: [14, 21, 37, 46] },
+      { pos: 23, expected: [6, 13, 29, 38] },
+      { pos: 15, expected: [5, 21, 30] },
+    ];
+
+    edgePositions.forEach(({ pos, expected }) => {
+      board = createEmptyBoard();
+      board[pos] = knight;
+      const moves = knight.possibleMoves(pos, board);
+
+      expect(moves).toEqual(expect.arrayContaining(expected));
+      expect(moves.length).toBe(expected.length);
+    });
+  });
+
+  it('should handle midpoints of top and bottom edges', () => {
+    const edgePositions = [
+      { pos: 3, expected: [13, 18, 9, 20] },
+      { pos: 4, expected: [10, 19, 21, 14] },
+      { pos: 59, expected: [44, 53, 42, 49] },
+      { pos: 60, expected: [43, 54, 45, 50] },
     ];
 
     edgePositions.forEach(({ pos, expected }) => {
