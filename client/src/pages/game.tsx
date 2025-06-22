6bed0412b0473fa7ff9202e5cf961248bee8079f
Terry0532 <t3rrygong@gmail.com>
2025-06-22 16:24:02 -0600

    ChessIconsCredit.tsx

--- client/src/pages/game.tsx
+++ client/src/pages/game.tsx
@@ -18,6 +18,7 @@ import {
 } from "../helpers/chessGameLogic";
 import GameInfo from "../components/GameInfo";
 import { createSocketEventHandlers } from "../helpers/socketEventHandlers";
+import ChessIconsCredit from "../components/ChessIconsCredit";
 
 const Game = ({ socket }: { socket: Socket }) => {
   const [gameState, dispatchGameAction] = useReducer(gameReducer, initialGameState);
@@ -387,27 +388,7 @@ const Game = ({ socket }: { socket: Socket }) => {
               dispatchGameAction={dispatchGameAction} 
             />
           </div>
-          <div className="icons-attribution">
-            <div>
-              {" "}
-              <small className={gameState.theme}>
-                {" "}
-                Chess Icons And Favicon (extracted) By en:User:Cburnett [
-                <a href="http://www.gnu.org/copyleft/fdl.html">GFDL</a>,{" "}
-                <a href="http://creativecommons.org/licenses/by-sa/3.0/">
-                  CC-BY-SA-3.0
-                </a>
-                ,{" "}
-                <a href="http://opensource.org/licenses/bsd-license.php">
-                  BSD
-                </a>{" "}
-                or <a href="http://www.gnu.org/licenses/gpl.html">GPL</a>],{" "}
-                <a href="https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces">
-                  via Wikimedia Commons
-                </a>{" "}
-              </small>
-            </div>
-          </div>
+          <ChessIconsCredit theme={gameState.theme} />
         </div>
       ) : (
         <div>
